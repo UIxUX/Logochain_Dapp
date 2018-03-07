f7a327227fd095a3ea259b9fd56534b1e99690fe
JulianPraest <julian.praest@folkwang-uni.de>
2018-03-07 23:05:27 +0100

    Started dividing HTML files into to ejs files.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var account = require('./routes/account')
 
 var app = express();
 
@@ -24,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+//account site
+app.use('/myaccount', account);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
