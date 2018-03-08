9d5b8086bcfbab728e02412dc1fbbeddfdab34e4
JulianPraest <julian.praest@folkwang-uni.de>
2018-03-08 23:32:54 +0100

    test branch - first commit.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+/* JSON test */
+function getDummyJSON() {
+    var json = '{ "h1": "Hallo", "h2": "Julian" }';
+    var jsonObject = JSON.parse(json);
+
+    return jsonObject;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("Get Request **********");
+  res.render('index', { h1: getDummyJSON().h1 });
 });
+
+
 
 module.exports = router;
