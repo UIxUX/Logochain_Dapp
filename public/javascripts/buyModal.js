c3ba1634b8a732a6d54fdfcedbdf12992bb0f44d
JulianPraest <julian.praest@folkwang-uni.de>
2018-04-05 20:57:08 +0200

    Preparation for Metamask integration. /2

--- public/javascripts/buyModal.js
+++ public/javascripts/buyModal.js
@@ -20,7 +20,7 @@ var buyModal = document.createElement('div');
 function fadeInBuyModal() {
     buyModal.innerHTML  =  '<div id="buyModal" class="round shadow modalView">\n' +
         '        <p>Purchase this Logo using Ether</p>\n' +
-        '        <p class="smalltext">Send the correct amount of Ether to the address below to purchase the Logo.</p>\n' +
+        '        <p class="smalltext">Using the Metamask extension, you can purchase this logo. </p>\n' +
         //'        <img class="submissionImg inSubmissionElement" width="90" height="90" class="img-circle" src="/images/testSvg.svg" alt="Your Logo Alt" onerror="this.src=\'your-alternative-image.png\'">\n' +
         '        <p class="smalltext" id="buyAddress">👉 0x3831f5e181fccaF8410FA61e12b59BAd933fb643</p>\n'
         '    </div>';
