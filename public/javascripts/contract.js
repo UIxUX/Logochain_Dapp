1141750f2be8b32e55e0f001b8b0831afaded26b
JulianPraest <julian.praest@folkwang-uni.de>
2018-04-09 20:37:02 +0200

    added new route for get purchasedata JSON.

--- public/javascripts/contract.js
+++ public/javascripts/contract.js
@@ -1,8 +1,143 @@
 //Test only for now.
 
-metaFrontAddress = "0x1d0d034f39c162eddd28934d85bd1cc5ba7bb1b2";
+metaFrontAddress = "0x3c184be2ffe6d8562c456b770c8a152fb54983fd";
 
 metaFrontAbi = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "query2",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "subIDsBought",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "sentValue",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "buyer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "query1",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "price",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "lastTransactionWasValid",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "author",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "name": "description",
+                "type": "string"
+            }
+        ],
+        "name": "newOraclizeQuery",
+        "type": "event"
+    },
     {
         "constant": false,
         "inputs": [
@@ -44,25 +179,20 @@ metaFrontAbi = [
         "type": "function"
     },
     {
-        "inputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    },
-    {
         "constant": false,
         "inputs": [
-            {
-                "name": "receiver",
-                "type": "address"
-            },
             {
                 "name": "subID",
                 "type": "string"
             }
         ],
         "name": "purchaseLogo",
-        "outputs": [],
+        "outputs": [
+            {
+                "name": "success",
+                "type": "bool"
+            }
+        ],
         "payable": true,
         "stateMutability": "payable",
         "type": "function"
